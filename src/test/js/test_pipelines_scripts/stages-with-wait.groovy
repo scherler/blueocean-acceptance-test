df7abc969243623ff96adc97131c3cf592d2cae0
Thorsten Scherler <scherler@gmail.com>
2016-07-14 02:18:16 +0200

    [karaokeMoreWait] prevent timeout for waiting to finish by shorten the pipeline

--- src/test/js/test_pipelines_scripts/stages-with-wait.groovy
+++ src/test/js/test_pipelines_scripts/stages-with-wait.groovy
@@ -5,14 +5,14 @@ node {
 node {
     stage 'Stage 2'
     parallel firstBranch: {
-        sh 'echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;'
+        sh 'echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;'
 
     }, secondBranch: {
-        sh 'echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;'
+        sh 'echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;'
     },
     failFast: true
 }
 node {
     stage 'fin'
-    sh 'echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;'
+    sh 'echo `date` fin;sleep 3; echo `date` fin;'
 }
